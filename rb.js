25c805a430ed80e4aec9eaa3aff65157a6c8ac11
br3nt <brent.jacobs+github@gmail.com>
2019-08-25 20:42:41 +0930

    Create rb.js
    Add initial functions and ideas for more

---
+++ rb.js
@@ -0,0 +1,21 @@
+// Array functions
+Array.prototype.each = Array.prototype.forEach
+Array.prototype.collect = Array.prototype.map
+// TODO:
+//    inject
+//    each_with_index
+
+// Object functions
+Object.prototype.keys = function() { return Object.keys(this) }
+Object.prototype.values = function() { return Object.values(this) }
+// TODO:
+//    each
+//    slice
+//    reject
+//    select
+//    values_at
+
+// string functions
+// TODO:
+//   blank
+//   empty
